661adcffd7ef1ab81a55d83584ddf92edadbd6c3
Oladiran <diran.johnson95@gmail.com>
2021-09-10 21:27:27 -0700

    Button Panels and Gui Title

--- src/FourButtons.java
+++ src/FourButtons.java
@@ -1,2 +1,11 @@
+import javax.swing.*;
+
 public class FourButtons {
+    private JPanel MainPanel;
+    private JPanel topLeftPanel;
+    private JPanel bottomLeftPanel;
+    private JPanel topRightPanel;
+    private JPanel bottomRightPanel;
+    private JPanel topPanel;
+    private JPanel bottomPanel;
 }
